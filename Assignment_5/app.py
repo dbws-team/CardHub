54c73f0be4e3e899f9d3194c1dbaa87829dad4bc
Aleksei Beliaev <abeliaev@clabsql.clamv.jacobs-university.de>
2023-11-02 20:39:13 +0100

    half working

--- Assignment_5/app.py
+++ Assignment_5/app.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask, render_template, request, Request
 from typing import List, Optional, Tuple
-from modules.Database import SqlException, Database
+from modules.Database import *
 
 app = Flask(__name__)
-db = Database(host=os.environ.get('DBWS_HOST'), user=os.environ.get('DBWS_USER'), password=os.environ.get('DBWS_PASSWORD'))
+db = Database(host=os.environ.get('DBWS_HOST'), user=os.environ.get('DBWS_USER'), password=os.environ.get('DBWS_PASSWORD'), database='Group-7')
 
 ENTITIES = ["photo_card", "text_card", "player", "creator", "cardset", "time_competition", "score_competition", "score_leaderboard", "time_leaderboard"]
 
@@ -12,7 +12,7 @@ ENTITIES_FIELDS = {"photo_card": ["url", "back_text"],
                    "text_card": ["front_text", "back_text"],
                    "player": ["username", "email", "password", "date_joined", "league"],
                    "creator": ["username", "email", "password", "date_joined"],
-                   "cardset": ["name"],
+                   "cardset": [],
                    "time_competition": ["start_time", "leaderboard_id"],
                    "score_competition": ["leaderboard_id"],
                    "score_leaderboard": ["user_id", "score", 'competition_id'],
@@ -65,7 +65,7 @@ def maintenance_page():
 
 @app.route('/maintenance/photo_card')
 def get_photo_card_input():
-    cardsets = {'cardset_ids': list(map(lambda x: x.id, db.select_cardsets()))}
+    cardsets = {'cardset_ids': list(map(lambda x: Cardset(x).id, db.select_cardsets()))}
     return render_template("entity.html", fields=ENTITIES_FIELDS['photo_card'],
                            entity_name='photo_card',
                            select_fields=RELATION_FIELDS['photo_card'],
@@ -73,7 +73,7 @@ def get_photo_card_input():
 
 @app.route('/maintenance/text_card')
 def get_text_card_input():
-    cardsets = {'cardset_ids': list(map(lambda x: x.id, db.select_cardsets()))}
+    cardsets = {'cardset_ids': list(map(lambda x: Cardset(x).id, db.select_cardsets()))}
     return render_template("entity.html", fields=ENTITIES_FIELDS['text_card'],
                            entity_name='text_card',
                            select_fields=RELATION_FIELDS['text_card'],
@@ -89,7 +89,8 @@ def get_creator_input():
 
 @app.route('/maintenance/cardset')
 def get_cardset_input():
-    creators = {'creator_ids': list(map(lambda x: x.id, db.select_creators()))}
+    print(db.select_creators())
+    creators = {'creator_ids': list(map(lambda x: Creator(*x).id, db.select_creators()))}
     return render_template("entity.html", fields=ENTITIES_FIELDS['cardset'],
                            entity_name='cardset',
                            select_fields=RELATION_FIELDS['cardset'],
@@ -97,28 +98,28 @@ def get_cardset_input():
 
 @app.route('/maintenance/time_competition')
 def get_time_competition_input():
-    players = {'player_ids': list(map(lambda x: x.id, db.select_players()))}
+    players = {'player_ids': list(map(lambda x: Player(*x).id, db.select_players()))}
     return render_template("entity.html", fields=ENTITIES_FIELDS['time_competition'], entity_name='time_competition',
                            select_fields=RELATION_FIELDS['time_competition'],
                            existed_relations=players)
 
 @app.route('/maintenance/score_competition')
 def get_score_competition_input():
-    players = {'player_ids': list(map(lambda x: x.id, db.select_players()))}
+    players = {'player_ids': list(map(lambda x: Player(*x).id, db.select_players()))}
     return render_template("entity.html", fields=ENTITIES_FIELDS['score_competition'], entity_name='score_competition',
                            select_fields=RELATION_FIELDS['score_competition'],
                            existed_relations=players)
 
 @app.route('/maintenance/score_leaderboard')
 def get_score_leaderboard_input():
-    competitions = {'competition_ids': list(map(lambda x: x.id, db.select_competitions()))}
+    competitions = {'competition_ids': list(map(lambda x: x[0], db.select_competitions()))}
     return render_template("entity.html", fields=ENTITIES_FIELDS['score_leaderboard'], entity_name='score_leaderboard',
                            select_fields=RELATION_FIELDS['score_leaderboard'],
                            existed_relations=competitions)
 
 @app.route('/maintenance/time_leaderboard')
 def get_time_leaderboard_input():
-    competitions = {'competition_ids': list(map(lambda x: x.id, db.select_competitions()))}
+    competitions = {'competition_ids': list(map(lambda x: x[0], db.select_competitions()))}
     return render_template("entity.html", fields=ENTITIES_FIELDS['time_leaderboard'], entity_name='time_leaderboard',
                            select_fields=RELATION_FIELDS['time_leaderboard'],
                            existed_relations=competitions)
--- Assignment_5/modules/Database.py
+++ Assignment_5/modules/Database.py
@@ -16,6 +16,7 @@ class SqlException(Exception):
 class Player:
     id: int
     username: str
+    password: str
     email: str
     date_joined: datetime.datetime
     league: str
@@ -24,6 +25,7 @@ class Player:
 class Creator:
     id: int
     username: str
+    password: str
     email: str
     date_joined: datetime.datetime
 
@@ -64,6 +66,9 @@ class ScoreLeaderboard:
     user_id: int
     score: int
 
+true = True
+false = False
+
 class Database:
     def __init__(self, user: str, password: str, host: str = 'localhost', port: int = 3306, database='dbws'):
         self.host = host
@@ -76,7 +81,7 @@ class Database:
         self.competitions = 0
         self.cards = 0
 
-    def execute_query(self, _query: str, context: str):
+    def execute_query(self, _query: str, context: str, flag: bool):
         try:
             connection = mysql.connector.connect(
                 host=self.host,
@@ -87,11 +92,14 @@ class Database:
 
             _query = f'{_query}'
             cursor.execute(_query)
-            res = cursor.fetchall()
-
-            cursor.close()
-            connection.close()
-            return res
+            if flag:
+                res = cursor.fetchall()
+                cursor.close()
+                connection.close()
+                return res
+            else:
+                cursor.close()
+                connection.close()
         except mysql.connector.Error as err:
             raise SqlException(error=err, context=context)
 
@@ -135,58 +143,58 @@ class Database:
         return f"INSERT INTO COMPETITIONS(LeaderboardId, CompetitionType) VALUES({leaderboard_id}, {competition_type});"
 
     def create_photo_card(self, front_photo_url, back_text):
-        self.execute_query(self.insert_card(back_text), 'Insert card')
+        self.execute_query(self.insert_card(back_text), 'Insert card', false)
         self.cards += 1
-        self.execute_query(self.insert_photo_card(self.cards, front_photo_url, back_text), 'Insert PhotoCard')
+        self.execute_query(self.insert_photo_card(self.cards, front_photo_url, back_text), 'Insert PhotoCard', false)
 
     def create_text_card(self, front_text, back_text):
-        self.execute_query(self.insert_card(back_text), 'Insert card')
+        self.execute_query(self.insert_card(back_text), 'Insert card', false)
         self.cards += 1
-        self.execute_query(self.insert_text_card(self.cards, front_text, back_text), 'Insert TextCard')
+        self.execute_query(self.insert_text_card(self.cards, front_text, back_text), 'Insert TextCard', false)
 
     def create_player(self, username, email, password, date_joined, league):
-        self.execute_query(self.insert_user(username, email, password, date_joined), 'Insert User')
+        self.execute_query(self.insert_user(username, email, password, date_joined), 'Insert User', false)
         self.users += 1
-        self.execute_query(self.insert_player(self.users, league), 'Insert Player')
+        self.execute_query(self.insert_player(self.users, league), 'Insert Player', false)
 
     def create_creator(self, username, email, password, date_joined):
-        self.execute_query(self.insert_user(username, email, password, date_joined), 'Insert User')
+        self.execute_query(self.insert_user(username, email, password, date_joined), 'Insert User', false)
         self.users += 1
-        self.execute_query(self.insert_creator(self.users), 'Insert Creator')
+        self.execute_query(self.insert_creator(self.users), 'Insert Creator', false)
 
     def create_cardset(self):
-        self.execute_query(self.insert_cardset(), 'Insert Cardset')
+        self.execute_query(self.insert_cardset(), 'Insert Cardset', false)
 
     def create_score_leaderboard(self, competition_id, users_scores_places):
-        self.execute_query(self.insert_leaderboard(competition_id), 'Insert Leaderboard')
+        self.execute_query(self.insert_leaderboard(competition_id), 'Insert Leaderboard', false)
         self.leaderboards += 1
         for user_id, score, place in users_scores_places:
-            self.execute_query(self.insert_score_leaderboards(self.leaderboards, user_id, score, place), 'Insert ScoreLeaderboard')
+            self.execute_query(self.insert_score_leaderboards(self.leaderboards, user_id, score, place), 'Insert ScoreLeaderboard', false)
 
     def create_time_leaderboard(self, competition_id, users_times_places):
-        self.execute_query(self.insert_leaderboard(competition_id), 'Insert Leaderboard')
+        self.execute_query(self.insert_leaderboard(competition_id), 'Insert Leaderboard', false)
         self.leaderboards += 1
         for user_id, time, place in users_times_places:
-            self.execute_query(self.insert_time_leaderboards(self.leaderboards, user_id, time, place), 'Insert TimeLeaderboard')
+            self.execute_query(self.insert_time_leaderboards(self.leaderboards, user_id, time, place), 'Insert TimeLeaderboard', false)
 
     def create_time_competition(self, leaderboard_id, start_time):
-        self.execute_query(self.insert_competiton(leaderboard_id, 'TIME'), 'Insert Competition')
+        self.execute_query(self.insert_competiton(leaderboard_id, 'TIME'), 'Insert Competition', false)
         self.competitions += 1
-        self.execute_query(self.insert_time_competitions(self.competitions, leaderboard_id, start_time), 'Insert TimeCompetition')
+        self.execute_query(self.insert_time_competitions(self.competitions, leaderboard_id, start_time), 'Insert TimeCompetition', false)
 
     def create_score_competition(self, leaderboard_id, start_time):
-        self.execute_query(self.insert_competiton(leaderboard_id, 'SCORE'), 'Insert Competition')
+        self.execute_query(self.insert_competiton(leaderboard_id, 'SCORE'), 'Insert Competition', false)
         self.competitions += 1
-        self.execute_query(self.insert_time_competitions(self.competitions, leaderboard_id, start_time), 'Insert ScoreCompetition')
+        self.execute_query(self.insert_time_competitions(self.competitions, leaderboard_id, start_time), 'Insert ScoreCompetition', false)
 
     def select_cardsets(self):
-        return self.execute_query("select * from CARDSETS", "Selecting cardsets")
+        return self.execute_query("select * from CARDSETS", "Selecting cardsets", true)
 
     def select_creators(self):
-        return self.execute_query("select * from CREATORS", "Selecting creators")
+        return self.execute_query("select * from USERS", "Selecting creators", true)
 
     def select_players(self):
-        return self.execute_query("select * from PLAYERS", "Selecting players")
+        return self.execute_query("select * from USERS", "Selecting players", true)
 
     def select_competitions(self):
-        return self.execute_query("select * from COMPETITIONS", "Selecting competitions")
+        return self.execute_query("select * from COMPETITIONS", "Selecting competitions", true)
